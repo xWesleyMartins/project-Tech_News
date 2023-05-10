8a704ed353bff685e79285323924260075850fc0
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-10 01:07:58 -0300

    completando req10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,11 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    response = find_news()
+    list_count = {}
+    for news in response:
+        list_count[news["category"]] = list_count.get(news["category"], 0) + 1
+    sorted_list = sorted(list_count.items(), key=lambda x: (-x[1], x[0]))
+    return [key for key, value in sorted_list][:5]
