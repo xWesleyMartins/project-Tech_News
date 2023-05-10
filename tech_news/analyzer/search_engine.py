8dac067804484e537b200a63611f567492dd1a22
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-09 23:10:13 -0300

    feat: completando req07 e req08

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,28 @@
+from tech_news.database import search_news
+from tech_news.database import get_collection
+import datetime
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    regex_query = {"title": {"$regex": title, "$options": "i"}}
+    news_searching = search_news(regex_query)
+
+    result = [(news["title"], news["url"]) for news in news_searching]
+    return result
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        dt = datetime.date.fromisoformat(date)
+        date_str = dt.strftime("%d/%m/%Y")
+        query = {"timestamp": date_str}
+        projection = {"_id": False, "title": True, "url": True}
+        result = get_collection().find(query, projection)
+        return [(item["title"], item["url"]) for item in result]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
