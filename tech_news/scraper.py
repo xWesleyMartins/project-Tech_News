14a49ed4644033161d877fa5dc9bb08fb7aebbf4
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-09 21:00:49 -0300

    completando req04 e req05

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
 import time
 import requests
 import parsel
+import bs4
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -30,9 +32,57 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    soup = bs4.BeautifulSoup(html_content, "html.parser")
+
+    url_element = soup.find("link", {"rel": "canonical"})
+    url = url_element["href"] if url_element else None
+
+    title_element = soup.find(class_="entry-title")
+    title = title_element.text.rstrip() if title_element else None
+
+    timestamp_element = soup.find(class_="meta-date")
+    timestamp = timestamp_element.text[:10] if timestamp_element else None
+
+    writer_element = soup.find(class_="author").find("a")
+    writer = writer_element.text if writer_element else None
+
+    reading_time_element = soup.find(class_="meta-reading-time")
+    reading_time = (
+        int(reading_time_element.text[:2]) if reading_time_element else None
+    )
+
+    summary_element = soup.find(class_="entry-content").find("p")
+    summary = summary_element.text.rstrip() if summary_element else None
+
+    category_element = soup.find(class_="category-style").find(class_="label")
+    category = category_element.text if category_element else None
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    news_posted = []
+    next_page_url = "https://blog.betrybe.com/"
+
+    while len(news_posted) < amount and next_page_url:
+        html_content = fetch(next_page_url)
+        url_list = scrape_updates(html_content)
+        next_page_url = scrape_next_page_link(html_content)
+
+        for url in url_list:
+            news_posted.append(scrape_news(fetch(url)))
+
+            if len(news_posted) == amount:
+                break
+
+    create_news(news_posted)
+    return news_posted[:amount]
